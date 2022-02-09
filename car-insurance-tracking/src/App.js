65bc5d00c9ae04965841d7da7797df733de160b6
fburaktrd <fburakturedi@hotmail.com>
2022-02-09 15:02:45 +0300

    Wrapper component added

--- car-insurance-tracking/src/App.js
+++ car-insurance-tracking/src/App.js
@@ -27,10 +27,8 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      
-      <div className="expenses">
       <Expenses comps= {expenses}/>
-      </div>
+      
     </div>
   );
 }
--- car-insurance-tracking/src/components/Expenses.js
+++ car-insurance-tracking/src/components/Expenses.js
@@ -1,12 +1,13 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from './Card'
 import './Expenses.css';
 
 function Expenses(props){
     const items = props.comps.map((comp)=> <ExpenseItem title={comp.title} date={comp.date} amount={comp.amount}/>)
     return (
-        <div className="expenses">
+        <Card className="expenses">
             {items}
-        </div>
+        </Card>
     );
 }
 
