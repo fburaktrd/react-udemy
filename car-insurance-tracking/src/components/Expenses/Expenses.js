76c426611dbbf68dcbdfe224c4eda5425587bbd8
fburaktrd <fburakturedi@hotmail.com>
2022-02-10 14:31:22 +0300

    rearranging functions

--- car-insurance-tracking/src/components/Expenses/Expenses.js
+++ car-insurance-tracking/src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card'
 import './Expenses.css';
 
-function Expenses(props){
+const Expenses = (props)=>{
     const items = props.comps.map((comp)=> <ExpenseItem title={comp.title} date={comp.date} amount={comp.amount}/>)
     return (
         <Card className="expenses">
