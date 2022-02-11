3605b2cb93fd1f8dd5bab073cd1df425593b3b80
fburaktrd <fburakturedi@hotmail.com>
2022-02-11 14:38:01 +0300

    year selection added

--- car-insurance-tracking/src/components/Expenses/Expenses.js
+++ car-insurance-tracking/src/components/Expenses/Expenses.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 const Expenses = (props)=>{
     const items = props.comps.map((comp)=> <ExpenseItem title={comp.title} date={comp.date} amount={comp.amount}/>)
+    
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler= (selectedYear)=>{
+        setFilteredYear(selectedYear);
+    };
     return (
+        
         <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter = {filterChangeHandler}/>
             {items}
         </Card>
+       
     );
 }
 
