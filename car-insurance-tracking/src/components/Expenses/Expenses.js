1f2a567f044367806218f17adf602fc6485bbea5
fburaktrd <fburakturedi@hotmail.com>
2022-02-11 19:18:42 +0300

    added chart

--- car-insurance-tracking/src/components/Expenses/Expenses.js
+++ car-insurance-tracking/src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props)=>{
     
@@ -25,6 +26,7 @@ const Expenses = (props)=>{
         <li>
         <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter = {filterChangeHandler}/>
+        <ExpensesChart expenses = {filteredExpenses}/>
         <ExpensesList items = {filteredExpenses}/>
         </Card>
         </li>
