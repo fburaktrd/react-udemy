5ff26048b381a3357e25f2b7e191ce0803f578d5
fburaktrd <fburakturedi@hotmail.com>
2022-02-15 19:25:55 +0300

    added UserList.js  component

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UsersList';
 
 function App() {
   return (
     <div>
       <AddUser/>
+      <UserList users={[]}/>
     </div>
   );
 }
