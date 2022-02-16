52e7de5dc2e5b28e363e5d1267316c97f467e15d
fburaktrd <fburakturedi@hotmail.com>
2022-02-16 22:37:22 +0300

    fixing bugs

--- practice-app/src/components/UI/ErrorModal.js
+++ practice-app/src/components/UI/ErrorModal.js
@@ -5,11 +5,11 @@ import Card from "./Card";
 import Button from "./Button";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdop} onClick={props.onConfirm}></div>;
+  return <div className={classes.backdrop} onClick={props.onConfirm}></div>;
 };
 
 const ModelOverlay = (props) => {
-  <Card className={classes.modal}>
+  return (<Card className={classes.modal}>
     <header className={classes.header}>
       <h2>{props.title}</h2>
     </header>
@@ -19,7 +19,7 @@ const ModelOverlay = (props) => {
     <footer className={classes.actions}>
       <Button onClick={props.onConfirm}>Okay</Button>
     </footer>
-  </Card>;
+  </Card>);
 };
 
 const ErrorModal = (props) => {
