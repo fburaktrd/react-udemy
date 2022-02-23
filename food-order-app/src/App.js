d11a0f3220f1872e4558783ea35b05feb449b5b4
fburaktrd <fburakturedi@hotmail.com>
2022-02-24 02:04:53 +0300

    Adding a Modal via a React Portal

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,10 +1,13 @@
 import { Fragment } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./Meals/Meals";
+
 
 function App() {
   return (
     <Fragment>
+      <Cart/>
       <Header/>
       <main>
       <Meals/>
