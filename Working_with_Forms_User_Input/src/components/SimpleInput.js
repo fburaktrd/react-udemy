834ed8e9f9ab095d9f361c75b8f8155cd689e025
fburaktrd <fburakturedi@hotmail.com>
2022-03-08 18:02:42 +0300

    renamed the folder and working with user inputs

---
+++ Working_with_Forms_User_Input/src/components/SimpleInput.js
@@ -0,0 +1,40 @@
+import { useRef, useState } from "react";
+
+const SimpleInput = (props) => {
+  
+  const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef();
+
+
+  const nameInputChangeHandler = event =>{
+    setEnteredName(event.target.value);
+  };
+
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    // nameInputRef.current.value = ''; SHOULDN'T USE THIS APPROACH
+    setEnteredName('');
+
+  };
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className='form-control'>
+        <label htmlFor='name'>Your Name</label>
+        <input ref={nameInputRef} onChange={nameInputChangeHandler} type='text' id='name' value={enteredName}/>
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
