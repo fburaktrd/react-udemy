510f7e35695f08dc03f0180d3e5e30041928f1d3
fburaktrd <fburakturedi@hotmail.com>
2022-03-12 14:19:28 +0300

    working with multiple slices

--- Redux/Redux_app/src/App.js
+++ Redux/Redux_app/src/App.js
@@ -1,9 +1,15 @@
+import { Fragment } from 'react';
+import Auth from './components/Auth';
 import Counter from './components/Counter';
-
+import Header from './components/Header';
 
 function App() {
   return (
+    <Fragment>
+    <Header/>
+    <Auth/>
     <Counter />
+    </Fragment>
   );
 }
 
